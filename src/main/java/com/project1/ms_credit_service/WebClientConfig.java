567e3ac2862da6115580eeeacc7277a6fdf23aa1
mauro-arcelles <mauroar1001@hotmail.com>
2025-02-11 12:21:49 -0500

    feature: use service base url from config server

--- src/main/java/com/project1/ms_credit_service/WebClientConfig.java
+++ src/main/java/com/project1/ms_credit_service/WebClientConfig.java
@@ -1,5 +1,6 @@
 package com.project1.ms_credit_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -7,8 +8,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 @Configuration
 public class WebClientConfig {
 
+    @Value("${customer.service.baseUrl}")
+    private String customerServiceBaseUrl;
+
     @Bean
-    public WebClient webClient() {
-        return WebClient.create("http://localhost:8090/api/v1");
+    public WebClient customerWebClient() {
+        return WebClient.builder()
+                .baseUrl(customerServiceBaseUrl)
+                .build();
     }
 }
